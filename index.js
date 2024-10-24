3c90f3f7072194f25987841bbaf6c451b0e9c903
Wayner10 <waynerwilson49@gmail.com>
2024-10-24 16:32:14 -0600

    24-10-24

--- index.js
+++ index.js
@@ -1,49 +1,41 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv"); // Importar dotenv para las variables de entorno
 
-dotenv.config();
+dotenv.config(); // Cargar variables de entorno desde el archivo .env
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 const placesRouter = require("./src/routes/places");
 const usersRouter = require("./src/routes/users");
 const categoriesRouter = require("./src/routes/categories");
 const regionsRouter = require("./src/routes/regions");
 const reviewsRouter = require("./src/routes/reviews");
-const place_typeRouter = require("./src/routes/other/Places_types");
-const authRouter = require("./src/routes/auth");
+const place_typeRouter = require("./src/routes/other/Places_types")
+const authRouter = require("./src/routes/auth"); 
 
-const allowedOrigins = ['http://localhost:3000', 'https://unravel-front.vercel.app'];
-
+// Configurar CORS para permitir solicitudes desde http://localhost:3000
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
+    origin: 'http://localhost:3000'
 }));
 
-// Manejar solicitudes preflight OPTIONS
-app.options('*', cors());
+app.use(express.json()); // Middleware para analizar cuerpos JSON
 
-app.use(express.json());
-
+// Rutas
 app.use("/api", placesRouter);
 app.use("/api", usersRouter);
 app.use("/api", categoriesRouter);
 app.use("/api", regionsRouter);
 app.use("/api", reviewsRouter);
-app.use("/api", place_typeRouter);
-app.use("/auth", authRouter);
+app.use("/api", place_typeRouter)
+app.use("/auth", authRouter); 
 
-// if (!process.env.RESEND_API_KEY) {
-//   throw `Abort: You need to define RESEND_API_KEY in the .env file.`;
-// }
+// Verificar la clave de la API antes de iniciar el servidor
+if (!process.env.RESEND_API_KEY) {
+  throw `Abort: You need to define RESEND_API_KEY in the .env file.`;
+}
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en el puerto ${port}`);
